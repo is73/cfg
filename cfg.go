1ddfba2170b304b7afad26ff76d9861c759f2c04
Ivan <suchy.ivan@gmail.com>
2016-04-08 11:37:05 +0200

    Added files via upload

---
+++ cfg.go
@@ -0,0 +1,35 @@
+package cfg
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"regexp"
+	"strings"
+)
+
+// Read reads config file and returns its contents
+// as map a.k.a associative array of keys and values
+func Read(fileName string) map[string]string {
+	f, err := os.Open(fileName)
+	if err != nil {
+		log.Panic("Config file error: ", err)
+	}
+	defer f.Close()
+
+	config := make(map[string]string, 10)
+	re := regexp.MustCompile(`[\s\t]+`)
+	lines := bufio.NewScanner(f)
+
+	for lines.Scan() {
+		line := strings.TrimSpace(lines.Text())
+		if !strings.HasPrefix(line, "#") && len(line) > 1 {
+			pair := re.Split(line, 2)
+			key := strings.TrimSpace(pair[0])
+			val := strings.TrimSpace(pair[1])
+			config[key] = val
+		}
+	}
+
+	return config
+}
---
+++ cfg_test.go
@@ -0,0 +1,24 @@
+package cfg
+
+import (
+	"testing"
+)
+
+func Test_Read(t *testing.T) {
+	var tests = []struct {
+		key string
+		val string
+	}{
+		{"key1", "value1"},
+		{"key2", "value2"},
+		{"key3", "value3"},
+		{"key4", "value4"},
+		{"key5", "value5 value5\tvalue5"},
+	}
+	config := Read("config.txt")
+	for _, test := range tests {
+		if test.val != config[test.key] {
+			t.Errorf("For %s expected %s, got %s", test.key, test.val, config[test.key])
+		}
+	}
+}
