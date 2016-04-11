e163d432722292cf0fcc3e9ddb9563fa53629d68
is73 <suchy.ivan@gmail.com>
2016-04-11 22:26:10 +0200

    Examples

--- example/main.go
+++ example/main.go
@@ -18,3 +18,20 @@ func main() {
     fmt.Printf("\nValue of key3 is: %s\n",
     	config["key3"])
 }
+
+/* Output:
+
+'key1':'value1'
+'key2':'value2'
+'key3':'value3 value3		value3'
+'user.id':'1'
+'user.name':'john'
+'user.surname':'doe'
+'smtp_port':'25'
+'smtp_password':'harDtoGueSs'
+'smtp_user':'info@example.com'
+'smtp_server':'smtp.example.com'
+
+Value of key3 is: value3 value3		value3
+
+*/
