71bdd4614f87bb113bf12bc0866933535392af91
is73 <suchy.ivan@gmail.com>
2016-04-12 20:47:07 +0200

    Tests edit

--- cfg_test.go
+++ cfg_test.go
@@ -12,13 +12,16 @@ func Test_Read(t *testing.T) {
 	}{
 		{"key1", "value1"},
 		{"key2", "value2"},
-		{"key3", "value3"},
-		{"key4", "value4"},
-		{"key5", "value5 value5\tvalue5"},
-		{"key6.user", "username"},
-		{"key6.db", "mydb"},
+		{"key3", "value3 value3\tvalue3"},
+		{"user.id", "1"},
+		{"user.name", "john"},
+		{"user.surname", "doe"},
+		{"smtp_server", "smtp.example.com"},
+		{"smtp_port", "25"},
+		{"smtp_user", "info@example.com"},
+		{"smtp_password", "harDtoGueSs"},
 	}
-	config, err := Read("config.txt", "")
+	config, err := Read("example/config.txt", "")
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -33,5 +36,5 @@ func Test_Read(t *testing.T) {
 }
 
 func Benchmark_Read(b *testing.B) {
-	_, _ = Read("config.txt", "")
+	_, _ = Read("example/config.txt", "")
 }
